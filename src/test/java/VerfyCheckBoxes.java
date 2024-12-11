fdab8e78e64e03bfddc6b4bfe4df93d2a2e3b60a
basivireddy45 <2301050106cse@gmail.com>
2024-12-11 21:26:36 +0530

    commit

---
+++ src/test/java/VerfyCheckBoxes.java
@@ -0,0 +1,36 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+    public class VerfyCheckBoxes {
+
+    public WebDriver driver;
+
+    @BeforeTest
+    public void launchBrowser() {
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void verifyCheckBoxIsSelected() {
+
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+        boolean checkBoxBefore = driver.findElement(By.xpath("(//form[@id='checkboxes']/input)[1]")).isSelected();
+        System.out.println("checkBoxBefore :: " + checkBoxBefore);
+        driver.findElement(By.xpath("(//form[@id='checkboxes']/input)[1]")).click();
+        boolean checkBoxAfter = driver.findElement(By.xpath("(//form[@id='checkboxes']/input)[1]")).isSelected();
+        System.out.println("checkBoxAfter :: " + checkBoxAfter);
+    }
+
+    @AfterTest
+    public void killSession() {
+        driver.close();
+    }
+}
